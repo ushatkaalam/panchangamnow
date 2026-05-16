ab8dc20d7760d8f50b14d94d46c1497b977973b8
ushatkaalam <ushashok@gmail.com>
2026-05-16 11:30:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -567,24 +567,6 @@ const ELEMENT_INDEX_STORE = {
   yogam: null,
   karanam: null
 };
-ui_labels_app {
-title_prefix: {
-  en: "Panchangam",
-  sa: "पञ्चाङ्गम्",
-  ta: "பஞ்சாங்கம்",
-  te: "పంచాంగం",
-  ka: "ಪಂಚಾಂಗ"
-},
-
-now: {
-  en: "Now",
-  sa: "इदानीम्",
-  ta: "இப்போது",
-  te: "ఇప్పుడు",
-  ka: "ಇದೀಗ"
-}
-
-};
 /***********************
  * COLOUR DEFINITIONS
  ***********************/
@@ -801,11 +783,10 @@ const formattedDateTime = nowLocal.toLocaleString("en-US", {
 });
 
 // Display current time
+document.getElementById("nowTime").innerHTML =
+  `<b>Current date & time:</b> ${formattedDateTime}`;
 
-  document.getElementById("appTitle").innerHTML =
-    `${getLang(ui_labels_app.title_prefix)} ${getLang(ui_labels_app.now)}`;
 
-  
 
 let GLOBAL_EXTRAS = {
   chandramanam: null,
